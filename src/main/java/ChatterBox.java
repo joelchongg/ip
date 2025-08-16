68b81dce6160ee24f9365befff344a228f4dcc0a
joelchongg <joelchongggg@gmail.com>
2025-08-16 20:27:26 +0800

    Rename Main Class
    Main class name (Duke) is generic.
    
    Renamed the main class name to ChatterBox and
    provided the skeletal code to simply greet user
    and exit.

---
+++ src/main/java/ChatterBox.java
@@ -0,0 +1,10 @@
+public class ChatterBox {
+
+    public static void main(String[] args) {
+        String startPhrase = "Hello! I'm ChatterBox!\nWhat can I do for you?";
+        System.out.println(startPhrase);
+
+        String endPhrase = "Bye. Hope to see you again soon!";
+        System.out.println(endPhrase);
+    }
+}
