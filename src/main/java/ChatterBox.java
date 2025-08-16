8b860f8939a5a4d4ff3c76eda288d3a5851260b2
joelchongg <joelchongggg@gmail.com>
2025-08-16 21:17:16 +0800

    Add support for list
    Lack of support to store and display user input
    
    Utilized the storage class to store user input,
    and display user input when the user inputs 'list'
    in the CLI.

--- src/main/java/ChatterBox.java
+++ src/main/java/ChatterBox.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ChatterBox {
 
     private static String name = "\nChatterBox: ";
+    private static Storage<String> storage = new Storage<>();
 
     private static void greetUser() {
         String startPhrase = name + "Hello! I'm ChatterBox\nChatterBox: What can I do for you?";
@@ -16,7 +17,13 @@ public class ChatterBox {
 
     private static void processUserInput(String userInput) {
         System.out.print(name);
-        System.out.println(userInput);
+        if (userInput.equals("list")) {
+            System.out.println();
+            storage.displayItems();
+        } else {
+            storage.addItem(userInput);
+            System.out.println("added: " + userInput);
+        }
     }
 
     private static void run() {
